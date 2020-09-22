779b55e43f1e371d74ecd354e74a30a768e0073b
Adrian Dmitroca <dmitroca.adrian@gmail.com>
2020-09-22 11:34:04 +0200

    copy

--- sidebars.js
+++ sidebars.js
@@ -3,6 +3,6 @@ module.exports = {
     'Getting Started': ['embeds-introduction', 'embeds-configurator', 'embeds-modular', 'events', 'localization', 'custom-translations'],
     Authentication: ['auth-full-api'],
     Libraries: ['library-html-client'],
-    Guides: ['guides-modular'],
+    Examples: ['guides-modular'],
   },
 };
